f451433d556efbecebac459429e94b80bdbd8274
mickfenneck <michele.sordo@gmail.com>
2017-12-19 13:28:45 +0100

    nuovi appunti

--- script.m
+++ script.m
@@ -17,8 +17,9 @@ for i=1:length(bonds)
     disp(bonds(i).price);
 end
 
-port = [{'btp1'},{'btp2'},{'btp3'},{'btp4'},...
-        {'btp5'},{'btp6'},{'btp7'},{'btp8'},{'btp9'}];
+port = [{'btp1'},{'btp2'},{'btp3'},...
+        {'btp4'},{'btp5'},{'btp6'},...
+        {'btp7'},{'btp8'},{'btp9'}];
     
 printBtpPlots(btp,port,btp.date(1),btp.date(end));
 %printBtpPlots(btp,port,btp.date(1),btp.date(end-100));
