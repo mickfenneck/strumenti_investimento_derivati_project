efca29937e7b657bcd63d2879aa1aa365fee39d7
Stegirsanov <you@example.com>
2017-12-23 17:00:31 +0100

    modifiche a create curve effettuate

--- createCurve.m
+++ createCurve.m
@@ -1,13 +1,12 @@
-function [ curva ] = createCurve( bonds,model )
+function [curva] = createCurve( bonds,model )
 %CREATECURVE Summary of this function goes here
 %   Detailed explanation goes here
 
-%inst={'Bond';'Bond';'Bond';'Bond';}
+inst=repelem({'Bond'},length(portCodes))';
+char=[datenum(btp.date) datenum(btp.maturity) btp.price]
 
-%char=[datenum(btp.date) datenum(btp.maturity) btp.price]
 if model == 'Bootstrap'
     curva =IRDataCurve.bootstrap('Zero',btp.date(1),inst,char,'InstrumentCouponRate',btp.coupon);
-%la curva ns vuole inst diversa
 elseif model == 'NelsonSiegel'
     instr=[btp.date datenum(btp.maturity) btp.price btp.coupon];
     curva= IRFunctionCurve.fitNelsonSiegel('Zero',btp.date(1), instr);
@@ -16,6 +15,7 @@ elseif model == 'Svensson'
 else
     error('Model deve contenere i seguenti valori: Bootstrap, NelsonSiegel, Svensson');
 end
+
 
 end
 
