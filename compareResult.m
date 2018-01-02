1dc0321bbbc46350f2a580017ace2a06a715d52d
eliascarparo <elia.scarparo@studenti.unitn.it>
2018-01-02 15:08:45 +0100

    confronto prezzi

--- compareResult.m
+++ compareResult.m
@@ -1,8 +1,12 @@
-function [ output_args ] = compareResult( titolo, valoreMkt, data, curva )
-%COMPARERESULT Confronta il valore di mercato del titolo alla data con
-%quello previsto dalla curva
-%   Detailed explanation goes here
-
-
+comparefunction [ comparison ] = compareResult(portCodes,valoreMkt,data,clean,dirty,coupon)
+%COMPARERESULT Confronta il valore di mercato del titolo alla data di 
+% osservazione con quello stimato con la curva. Il valore di mkt reale va
+% specificato dall'utente prima di usare la funzione.
+% La funzione si propone di creare una tabella riportante i prezzi teorici
+% clean e dirty e il prezzo reale osservato.
+comparison=table(clean,dirty,valoreMkt,'VariableNames',{'clean',...
+    'dirty','valore_reale'},'RowNames',portCodes)
+disp(comparison)
+cfplot(btp.date,btp.coupon,'ShowAmnt',)
 end
 
