bf0dd8d913e59dcc2169cad69f77f500890cf73c
Stegirsanov <you@example.com>
2018-01-04 12:34:43 +0100

    funzione plot forward curve

--- plotForwardCurve.m
+++ plotForwardCurve.m
@@ -1,7 +1,20 @@
-function [ output_args ] = plotForwardCurve( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ forward ] = plotForwardCurve( bonds, curve, startDate, endDate, color)
+%PLOTFORWARDCURVE Summary of this function goes here
 
+%% controllare che startDate e end Date siano nell'intervallo accettabile
 
+%% plot della curva
+figure
+point= datenum(startDate):180:datenum(endDate);
+yield = bndyield(bonds.prices,bonds.coupon,bonds.date,bonds.maturity);
+plot(point, getForwardRates(curve, point),color)
+title('Curva dei tassi BTP al 15-Sep-2017')
+xlabel('Maturity')
+ylabel('Yield')
+hold on
+scatter(datenum(bonds.maturity),yield,'g')
+datetick('x')
+
+end
 end
 
