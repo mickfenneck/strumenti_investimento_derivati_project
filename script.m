3bd7f89139f61c5b0539364cd8bd88126b681d88
mickfenneck <michele.sordo@gmail.com>
2018-01-09 14:44:17 +0100

    pulizia2

--- script.m
+++ script.m
@@ -10,7 +10,7 @@ load data/bonds.mat
 
 dateSettlement = '17-Nov-2017';
 portCodes = [{'btp1'},{'btp2'},{'btp7'},{'btp9'}];
-portValues = [1000;1000;1000;1000];
+portValues = [1000;500;3000;2000];
 model1 = 'Bootstrap';
 model2 = 'NelsonSiegel';
 model3 = 'Svensson';
@@ -25,37 +25,3 @@ disp(port);
 difference = comparePortfolio(port);
 disp('Portfolio difference');
 disp(difference);
-% for i = 1:length(portCodes)
-%     disp(portCodes(i));
-% end
-%port = bonds({'btp1','btp3'},:);
-
-%port = bonds({'btp1','btp3'},:)
-
-
-
-% data = [{'btp1'},{'btp2'};
-%         0.012,0.045;
-%         '12-Aug-2028','12-Aug-2038'];
-
-% % creo due bond
-% a = bond(12,'12-Aug-2018','12-Aug-2018',0.13);
-% b = bond(13,'12-Aug-2018','12-Aug-2018',0.13);
-% c = bond(13,'12-Aug-2018','12-Aug-2018',0.13);
-% 
-% %= bond_portfolio(a,b,c);
-% disp(port);
-% 
-% bonds = [a b];
-% 
-% for i=1:length(bonds)
-%     disp(bonds(i).price);
-% end
-% 
-% port = [{'btp1'},{'btp2'},{'btp3'},...
-%         {'btp4'},{'btp5'},{'btp6'},...
-%         {'btp7'},{'btp8'},{'btp9'}];
-%     
-% printBtpPlots(btp,port,btp.date(1),btp.date(end));
-% %printBtpPlots(btp,port,btp.date(1),btp.date(end-100));
-
