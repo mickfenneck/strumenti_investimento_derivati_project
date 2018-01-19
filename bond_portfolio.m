b58cb21e51e8a60782c87f2218d463174ee50cab
eliascarparo <elia.scarparo@studenti.unitn.it>
2018-01-19 23:39:49 +0100

    commento classe

--- bond_portfolio.m
+++ bond_portfolio.m
@@ -1,15 +1,28 @@
 classdef bond_portfolio
-    %BOND_PORTFOLIO Summary of this class goes here
-    %   Detailed explanation goes here
+    
+    % BOND_PORTFOLIO: bond_portfolio è una classe utilizzata per identifi-
+    % care un tipo di dato astratto: in questo caso il portafoglio composto
+    % da bond. 
     
     properties
         Portfolio           %bond portfolio table
     end
+    
+    % Come istanze e attributi della classe abbiamo utilizzato gli oggetti:
+    % bond_portfolio:Valore teorico portfolio
+    % ValoreMercato:Valore reale portfolio
+    % DifferenzaMercatoTeorico: differenza tra v.reale e v.teorico
+    
     properties (Dependent)
         ValoreTeorico               %Valore teorico portfolio
         ValoreMercato                 %Valore reale portfolio
         DifferenzaMercatoTeorico      %differenza tra v.reale e v.teorico
     end
+    
+    % I seguenti metodi sono condivisi da tutti gli oggetti creati e hanno
+    % lo scopo di calcolare il valore reale e teorico del portafoglio
+    % oltre alla differenza tra v.reale e v.teorico ed succesivamente
+    % assegnarlo agli oggetti che compongono le istanze della classe.
     
     methods
         function obj = bond_portfolio(port)
