6559df31d176627151d0a091ae25becb4cfd806f
eliascarparo <elia.scarparo@studenti.unitn.it>
2018-01-19 23:17:43 +0100

    commento createPortfolio

--- createPortfolio.m
+++ createPortfolio.m
@@ -1,5 +1,21 @@
 function [ port ] = createPortfolio(bonds,port)
-% createPortfolio compone il 
+% "createPortfolio" compone una tabella composta dei titoli che l'utente
+% vuole avere in portafoglio. Questa funzione crea una tabella data
+% dall'unione di due tabelle:
+% "port" tabella indicante il valore nominale dei titoli che l'utente desi-
+% dera avere in portafoglio e bonds, cioè la tabella creata mediante la
+% funzione mergeData.
+% L'output è una tabella di sette colonne e tante righe quanti sono i bonds
+% di cui si compone il portafoglio. Le colonne riportano il valore per ogni
+% titolo dei valori di:
+% value: valore nominale di ciascun titolo;
+% maturity;
+% coupon rate;
+% descrizione;
+% dateSettlement;
+% instrument: tipologia di strumento, nel nostro caso Bonds;
+% market price.
+
 port = [port bonds(port.Properties.RowNames',:)];
 
 end
